3b2a208647adfbd4ba06dcefd93fd1e51101862f
AndreiMarianPatru <s193300@uos.ac.uk>
2019-02-09 10:55:26 +0000

    Game Prototype
    created a Game Prototype, red objects will reset the scene, green one will take you to the next level (when it will be one), player will be able to move only after hitting an object

--- Programming/New Unity Project/Assets/move.cs
+++ Programming/New Unity Project/Assets/move.cs
@@ -2,44 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move : MonoBehaviour {
+public class move : MonoBehaviour
+{
 
     // Use this for initialization
     public Rigidbody2D rb;
     public float thrust;
-    void Start () {
+    public bool ableToMove;
+
+
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
+        ableToMove = true;
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
+
+    // Update is called once per frame
+    void Update()
+    {
+
 
 
     }
     private void FixedUpdate()
     {
-        if (Input.GetKey("a"))
+
+        if (ableToMove == true)
         {
-            rb.AddForce(Vector3.left* thrust * Time.deltaTime );
+            if (Input.GetKey("a"))
+            {
+                rb.AddForce(Vector3.left * thrust * Time.deltaTime, ForceMode2D.Impulse);
+                ableToMove = false;
+
+            }
+            if (Input.GetKey("d"))
+            {
+                rb.AddForce(Vector3.right * thrust * Time.deltaTime, ForceMode2D.Impulse);
+                ableToMove = false;
+
+            }
+            if (Input.GetKey("w"))
+            {
+                rb.AddForce(Vector3.up * thrust / 100);
+                rb.AddForce(Vector3.up * thrust * Time.deltaTime, ForceMode2D.Impulse);
+                ableToMove = false;
+
+            }
+            if (Input.GetKey("s"))
+            {
+                rb.AddForce(Vector3.down * thrust * Time.deltaTime, ForceMode2D.Impulse);
+                ableToMove = false;
+
+            }
 
         }
-        if (Input.GetKey("d"))
-        {
-            rb.AddForce(Vector3.right * thrust * Time.deltaTime );
+        else
+            Debug.Log("can't move");
 
-        }
-        if (Input.GetKey("w"))
-        {
-            //rb.AddForce(Vector3.up * thrust);
-            rb.AddForce(Vector3.up * thrust/100  );
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        ableToMove = true;
 
-        }
-        if (Input.GetKey("s"))
-        {
-            //rb.AddForce(Vector3.down * thrust);
-            rb.AddForce(Vector3.down * thrust/100 );
 
-        }
     }
 }
