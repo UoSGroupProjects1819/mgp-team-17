4903da5c8f8d4ff9255e325585ea91218ee16d12
Andrei Marian Patru <andreiuttzzumarian00@gmail.com>
2019-02-20 14:54:31 +0000

    project

---
+++ Game Project/Master Project/Assets/scripts/AImovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AImovement : MonoBehaviour
+{
+    public int target;
+    public Vector3[] markers;
+    public float speed;
+
+
+    bool b;
+    int i;
+    void Start()
+    {
+
+        //Put however many Empty game objects in your scene for position markers and name them "marker1" and "marker2" and "marker3"
+
+        b = false;
+        i = 0;
+        while (!b) { if (GameObject.Find("marker" + i)) { i++; } else { b = true; } }
+        markers = new Vector3[i];
+        while (i > 0)
+        {
+            i--; markers[i] = GameObject.Find("marker" + i).transform.position;
+
+        }
+
+    }
+
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, markers[target], speed );
+        if (transform.position == markers[target])
+        {
+            target++;
+            if (target == markers.Length) { target = 0; }
+        }
+
+    }
+}
---
+++ Game Project/Master Project/Assets/scripts/move.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    // Use this for initialization
+    public Rigidbody2D rb;
+    public float thrust;
+    public bool ableToMove;
+    bool disableW = false;
+    bool disableA = false;
+    bool disableS = false;
+    bool disableD = false;
+
+    void Start()
+    {
+        
+        ableToMove = true;
+       
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var speed = rb.velocity.magnitude;
+
+        if (speed > 0.5)
+        {
+            ableToMove = false;
+        }
+        else
+        {
+            ableToMove = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (ableToMove == true)
+        {
+            if (Input.GetKey("a")&& disableA==false)
+            {
+                rb.AddForce(Vector3.left * thrust * Time.deltaTime, ForceMode2D.Impulse);
+                ableToMove = false;
+                disableA = true;
+                disableW = false;
+                disableS = false;
+                disableD = false;
+                return;
+
+            }
+            if (Input.GetKey("d") && disableD == false)
+            {
+                rb.AddForce(Vector3.right * thrust * Time.deltaTime, ForceMode2D.Impulse);
+                ableToMove = false;
+                disableA = false;
+                disableW = false;
+                disableS = false;
+                disableD = true;
+                return;
+
+
+            }
+            if (Input.GetKey("w") && disableW == false)
+            {
+                
+                rb.AddForce(Vector3.up * thrust * Time.deltaTime, ForceMode2D.Impulse);
+                ableToMove = false;
+                disableA = false;
+                disableW = true;
+                disableS = false;
+                disableD = false;
+                return;
+
+
+            }
+            if (Input.GetKey("s") && disableS == false)
+            {
+                rb.AddForce(Vector3.down * thrust * Time.deltaTime, ForceMode2D.Impulse);
+                ableToMove = false;
+                disableA = false;
+                disableW = false;
+                disableS = true;
+                disableD = false;
+                return;
+
+            }
+
+        }
+        else
+            Debug.Log("can't move");
+
+    }
+}
