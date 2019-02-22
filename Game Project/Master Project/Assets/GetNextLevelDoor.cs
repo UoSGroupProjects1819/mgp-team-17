ff2ef1a454381105ca73184cf5dec542e5c91985
AndreiMarianPatru <s193300@uos.ac.uk>
2019-02-22 22:04:48 +0000

    added  next level key and next level prefab

---
+++ Game Project/Master Project/Assets/GetNextLevelDoor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetNextLevelDoor : MonoBehaviour
+{
+    public Transform key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "player")
+        {
+
+            Instantiate(key, new Vector3(20, 5, 0), Quaternion.identity);
+        }
+
+
+    }
+}
