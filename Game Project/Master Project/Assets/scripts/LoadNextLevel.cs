70a9afbe46731c6a2a66cb189164830196b82390
Yurii Skorobogatko <43639323+ShmigabornDungiedoo@users.noreply.github.com>
2019-02-27 13:58:00 +0000

    boi2

--- Game Project/Master Project/Assets/scripts/LoadNextLevel.cs
+++ Game Project/Master Project/Assets/scripts/LoadNextLevel.cs
@@ -5,23 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class LoadNextLevel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    void OnTriggerEnter2D(Collision2D collision)
+   
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
 
-            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex+1 );
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1 );
+            Debug.Log("boye");
         }
 
 
