344b8821d989d92e4c38c64ce93f45c899f79b5c
fred-wright <fredwright2000@live.co.uk>
2019-03-07 13:12:43 +0000

    Update GetNextLevelDoor.cs

--- Game Project/Master Project/Assets/scripts/GetNextLevelDoor.cs
+++ Game Project/Master Project/Assets/scripts/GetNextLevelDoor.cs
@@ -8,13 +8,18 @@ public class GetNextLevelDoor : MonoBehaviour
     public GameObject wallToDestroy;
     public Vector3 NextLevelDoorCoords;
     public GameObject wallToActivate;
+    public bool doYouWantToSpawnAnExit;
      
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             GameManager.keys += 1;
-            //Instantiate(key, NextLevelDoorCoords, Quaternion.identity);
+            if (doYouWantToSpawnAnExit)
+            {
+                Instantiate(key, NextLevelDoorCoords, Quaternion.identity);
+            }
+            
             Destroy(gameObject);
             Destroy(wallToDestroy);
 
