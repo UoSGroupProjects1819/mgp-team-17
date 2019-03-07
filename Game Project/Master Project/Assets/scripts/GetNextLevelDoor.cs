65e2e50e06b5b772a2eba3bfe86a68e2c7bce756
fred-wright <fredwright2000@live.co.uk>
2019-03-07 13:08:49 +0000

    lv3 tweaks

--- Game Project/Master Project/Assets/scripts/GetNextLevelDoor.cs
+++ Game Project/Master Project/Assets/scripts/GetNextLevelDoor.cs
@@ -7,15 +7,18 @@ public class GetNextLevelDoor : MonoBehaviour
     public GameObject key;
     public GameObject wallToDestroy;
     public Vector3 NextLevelDoorCoords;
+    public GameObject wallToActivate;
      
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             GameManager.keys += 1;
-            Instantiate(key, NextLevelDoorCoords, Quaternion.identity);
-            Destroy(wallToDestroy);
+            //Instantiate(key, NextLevelDoorCoords, Quaternion.identity);
             Destroy(gameObject);
+            Destroy(wallToDestroy);
+
+            wallToActivate.SetActive(true);
         }
 
 
