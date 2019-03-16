e4d936011532ff144548febfff2304b17c1e4453
AndreiMarianPatru <s193300@uos.ac.uk>
2019-03-16 18:45:49 +0000

    teleport mechanic added

---
+++ Game Project/Master Project/Assets/Teleport.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject target;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Vector3 posi = target.gameObject.transform.position;
+            collision.gameObject.transform.position = posi;
+            Debug.Log("ana");
+        }
+    }
+}
