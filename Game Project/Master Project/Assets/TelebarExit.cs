d4a60778814dc88f45a543d851f616bcfb478b74
AndreiMarianPatru <s193300@uos.ac.uk>
2019-03-23 16:08:22 +0000

    Revert "Revert "teleport bars(not finished)""
    This reverts commit 4ab1494c7bc72eab84058de8dd561ea34e7378e3.

---
+++ Game Project/Master Project/Assets/TelebarExit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelebarExit : MonoBehaviour
+{
+    GameObject teleport;
+    private void Start()
+    {
+        teleport = GameObject.Find("Teleporter-exit");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 pos = teleport.transform.position;
+        pos.y += 1.5f;
+
+        gameObject.transform.position = pos;
+    }
+}
+
---
+++ Game Project/Master Project/Assets/scripts/telebarEnter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class telebarEnter : MonoBehaviour
+    
+{
+    GameObject teleport;
+    private void Start()
+    {
+        teleport = GameObject.Find("Teleporter-enter");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        Vector3 pos = teleport.transform.position;
+        pos.y += 1.5f;
+
+        gameObject.transform.position = pos;
+    }
+}
