2802e3509f45e2772c55a8f8909ac28be6efb438
AndreiMarianPatru <andrei@gmail.com>
2019-03-27 13:32:08 +0000

    coins update

--- Game Project/Master Project/Assets/scripts/coins.cs
+++ Game Project/Master Project/Assets/scripts/coins.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class coins : MonoBehaviour
 {
-    
-   
-   
+
+
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -14,6 +14,7 @@ public class coins : MonoBehaviour
             GameManager.timeLeft += 5f;
             GameManager.coins += 1;
             Destroy(gameObject);
+            other.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, this.GetComponent<SpriteRenderer>().color.a + 0.2f);
 
 
         }
