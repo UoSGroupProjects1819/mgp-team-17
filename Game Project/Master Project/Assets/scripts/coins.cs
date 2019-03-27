b0c7bbef557da8ea84fe063ab83511f596126437
AndreiMarianPatru <andrei@gmail.com>
2019-03-27 13:54:34 +0000

    coins update

--- Game Project/Master Project/Assets/scripts/coins.cs
+++ Game Project/Master Project/Assets/scripts/coins.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class coins : MonoBehaviour
 {
+    private void Update()
+    {
+        
+        Debug.Log(GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>().color.a);
+    }
 
 
 
@@ -13,8 +18,9 @@ public class coins : MonoBehaviour
         {
             GameManager.timeLeft += 5f;
             GameManager.coins += 1;
+            other.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, other.GetComponent<SpriteRenderer>().color.a + 0.5f);
             Destroy(gameObject);
-            other.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, this.GetComponent<SpriteRenderer>().color.a + 0.2f);
+            
 
 
         }
